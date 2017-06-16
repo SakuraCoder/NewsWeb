f3614f2fd68aaee5659161525b04cf4aeffe4ba5
SakuraCoder <632588258@qq.com>
2017-06-17 02:19:47 +0800

    add all news in content page

--- routes/content.js
+++ routes/content.js
@@ -8,9 +8,25 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     var newsContent = new News({newsid: req.query.newsid});
     newsContent.getNewsById(function(err,result){
+        console.log(result[0].title);
+        console.log(result[0].newsid);
         var newslist = new News({channel:'新闻'});
         newslist.newsInfo(function(err, result2){
-            res.render('content', {data: result, adata: result2});
+            var d1_list = new News({channel:'财经'});
+            d1_list.newsInfo(function(err, result3){
+                var d2_list = new News({channel:'体育'});
+                d2_list.newsInfo(function(err, result4){
+                    var d3_list = new News({channel:'娱乐'});
+                    d3_list.newsInfo(function(err, result5){
+                        var d4_list = new News({channel:'军事'});
+                        d4_list.newsInfo(function(err, result6){
+                            res.render('content', {data: result, adata: result2, d1:result3, d2:result4, d3:result5, d4:result6});
+                        });
+                    });
+
+                });
+
+            });
         });
     });
 });
